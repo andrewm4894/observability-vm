310b04005f316b365be357511b047ef16125b146
Andrew Maguire <andrewm4894@gmail.com>
2023-08-22 12:10:55 +0100

    add dev box

---
+++ terraform/gcp-compute-instances-dev.tf
@@ -0,0 +1,47 @@
+########################################
+# dev
+########################################
+#/*
+# make a static ip to use for this vm
+resource "google_compute_address" "dev" {
+  name = "dev"
+}
+
+# define the instance
+resource "google_compute_instance" "dev" {
+  name                      = "dev"
+  machine_type              = "n1-standard-1"
+  zone                      = var.gcp_zone
+  tags                      = ["dev"]
+  allow_stopping_for_update = true
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.ubuntu_2004.self_link
+      size  = 20
+      type  = "pd-standard"
+    }
+  }
+  network_interface {
+    network = google_compute_network.mynetwork.name
+    access_config {
+      nat_ip = google_compute_address.dev.address
+    }
+  }
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro", "cloud-platform"]
+  }
+  metadata_startup_script = templatefile("scripts/dev.sh", {
+    prepare : templatefile("scripts/src/prepare.sh", {
+      dummy-secret : data.google_secret_manager_secret_version.dummy_secret_read.secret_data
+    }),
+    install-stress-ng : file("scripts/src/install-stress-ng.sh"),
+    install-docker : file("scripts/src/install-docker.sh"),
+    install-netdata : templatefile("scripts/src/install-netdata.sh", {
+      netdata-fork : "kickstart",
+      netdata-branch : "",
+      netdata-claim-token : "",
+      netdata-claim-url : "",
+    }),
+  })
+}
+*/
---
+++ terraform/scripts/dev.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+echo "######################################"
+echo "# dev.sh"
+echo "######################################" 
+
+# prepare
+
+${prepare}
+
+# install
+
+${install-stress-ng}
+${install-docker}
+${install-netdata}
+
+# configure
+
+${configure-cronjobs}
+
+echo 'creating netdata.conf'
+sudo cat <<EOT > /etc/netdata/netdata.conf
+[ml]
+    enable statistics charts = yes
+EOT
+
+sudo systemctl restart netdata
+
+echo "######################################"
+echo "# FINISHED!!!"
+echo "######################################"
