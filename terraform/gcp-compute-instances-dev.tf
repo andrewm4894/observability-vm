631d34fcf88b6d81d8fe49ea75f35e8f16e60df2
Andrew Maguire <andrewm4894@gmail.com>
2023-09-14 17:24:19 +0100

    add 48hours test node

--- terraform/gcp-compute-instances-dev.tf
+++ terraform/gcp-compute-instances-dev.tf
@@ -1,7 +1,7 @@
 ########################################
 # dev
 ########################################
-/*
+#/*
 # make a static ip to use for this vm
 resource "google_compute_address" "dev" {
   name = "dev"
@@ -37,11 +37,59 @@ resource "google_compute_instance" "dev" {
     install-stress-ng : file("scripts/src/install-stress-ng.sh"),
     install-docker : file("scripts/src/install-docker.sh"),
     install-netdata : templatefile("scripts/src/install-netdata.sh", {
-      netdata-fork : "andrewm4894/netdata",
-      netdata-branch : "add-1min-node-ar-alert",
+      netdata-fork : "netdata/netdata",
+      netdata-branch : "master",
       netdata-claim-token : var.netdata_claim_token,
       netdata-claim-url : var.netdata_claim_url,
     }),
   })
 }
-*/
+#*/
+
+########################################
+# dev48hours
+########################################
+#/*
+# make a static ip to use for this vm
+resource "google_compute_address" "dev48hours" {
+  name = "dev48hours"
+}
+
+# define the instance
+resource "google_compute_instance" "dev48hours" {
+  name                      = "dev48hours"
+  machine_type              = "n1-standard-1"
+  zone                      = var.gcp_zone
+  tags                      = ["dev"]
+  allow_stopping_for_update = true
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.ubuntu_2004.self_link
+      size  = 20
+      type  = "pd-standard"
+    }
+  }
+  network_interface {
+    network = google_compute_network.mynetwork.name
+    access_config {
+      nat_ip = google_compute_address.dev48hours.address
+    }
+  }
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro", "cloud-platform"]
+  }
+  metadata_startup_script = templatefile("scripts/dev.sh", {
+    prepare : templatefile("scripts/src/prepare.sh", {
+      dummy-secret : data.google_secret_manager_secret_version.dummy_secret_read.secret_data
+    }),
+    install-stress-ng : file("scripts/src/install-stress-ng.sh"),
+    install-docker : file("scripts/src/install-docker.sh"),
+    install-netdata : templatefile("scripts/src/install-netdata.sh", {
+      netdata-fork : "andrewm4894/netdata",
+      netdata-branch : "ml48hours",
+      netdata-claim-token : var.netdata_claim_token,
+      netdata-claim-url : var.netdata_claim_url,
+    }),
+  })
+}
+#*/
