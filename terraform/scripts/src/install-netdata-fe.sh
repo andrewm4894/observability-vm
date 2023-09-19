36ec7039226010365083d90664f5d79d54aa9ffc
Andrew Maguire <andrewm4894@gmail.com>
2023-09-19 16:54:44 +0100

    pull agent script from a gist instead

--- terraform/scripts/src/install-netdata-fe.sh
+++ terraform/scripts/src/install-netdata-fe.sh
@@ -26,67 +26,11 @@ yarn && sudo yarn to-cloud
 cd /src/cloud-frontend
 
 # make agent.sh script 
-cat <<EOT > /src/cloud-frontend/agent.sh
-#!/usr/bin/env bash
+# curl from https://gist.github.com/andrewm4894/8e44678923b92a292ec8bdbf858ae9de
+curl -sS https://gist.githubusercontent.com/andrewm4894/8e44678923b92a292ec8bdbf858ae9de/raw/agent.sh > /src/cloud-frontend/agent.sh
 
-scope="production"
-
-# stop on all errors
-set -e
-
-if [ \$UID -ne 0 ]
-then
-	echo >&2 "Hey! sudo me: sudo ${0}"
-	exit 1
-fi
-
-echo >&2 "Finding the local netdata web directory..."
-webdir=$(curl -sS 'http://localhost:19999/netdata.conf' |\
-	sed -n '/\[directories\]/,/^ *$/p' |\
-	grep -E '.*[[:space:]]web[[:space:]]*=[[:space:]]' |\
-	sed 's/.*[[:space:]]web[[:space:]]*=[[:space:]]\(.*\)/\1/')
-
-if [ -z "$${webdir}" -o ! -d "$${webdir}" ]
-then
-	echo >&2 "Cannot find netdata web directory"
-	exit 1
-fi
-
-echo >&2 "Netdata web directory is '$${webdir}'"
-
-echo >&2 "Running yarn install..."
-yarn install
-
-echo >&2 "Running yarn build..."
-ENV="$${scope}" yarn build
-
-echo >&2 "Copying dashboard files to '$${webdir}"
-
-[ ! -d $${webdir}/v1 ] && mkdir -p $${webdir}/v1
-[ ! -d $${webdir}/v2 ] && mkdir -p $${webdir}/v2
-
-#version="$(git describe --tag | cut -d '-' -f 1)"
-#
-cp -R dist/* $${webdir}/v2/
-cp dist/registry-hello.html $${webdir}/
-cp dist/registry-access.html $${webdir}/
-cp dist/registry-alert-redirect.html $${webdir}/
-cp --no-clobber $${webdir}/index.html $${webdir}/v1/index.html || echo >/dev/null
-cp dist/agent.html $${webdir}/index.html
-cp dist/local-agent.html $${webdir}/v2/index.html
-
-echo >&2 "CREATING TAR in /tmp/agent-varlibnetdataweb.tar.gz ..."
-[ -f /tmp/agent-varlibnetdataweb.tar.gz ] && rm -f /tmp/agent-varlibnetdataweb.tar.gz
-old="$(pwd)"
-cd "$${webdir}" || exit 1
-tar -zcpf /tmp/agent-varlibnetdataweb.tar.gz v2/* registry-hello.html registry-access.html registry-alert-redirect.html index.html || exit 1
-cd "$${old}" || exit 1
-
-#ENV="agent" yarn start
-
-#echo >&2 "RUNNING..."
-#sudo yarn dev-$${scope}
-EOT
-
+# make script executable
 chmod +x /src/cloud-frontend/agent.sh
+
+# run script
 sudo /src/cloud-frontend/agent.sh
