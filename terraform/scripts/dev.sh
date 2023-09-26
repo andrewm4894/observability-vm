f38e7164bf9f4f142af1749e1012bf7ed42f9094
Andrew Maguire <andrewm4894@gmail.com>
2023-09-26 20:20:32 +0100

    dev

--- terraform/scripts/dev.sh
+++ terraform/scripts/dev.sh
@@ -10,24 +10,24 @@ ${prepare}
 
 # install
 
-${install-gh}
-${install-node}
-${install-yarn}
-${install-stress-ng}
+#$#{install-gh}
+#$#{install-node}
+#$#{install-yarn}
+#$#{install-stress-ng}
 ${install-docker}
-${install-netdata}
-${install-netdata-fe}
+#$#{install-netdata}
+#$#{install-netdata-fe}
 
 # configure
 
-echo 'creating netdata.conf'
-sudo cat <<EOT > /etc/netdata/netdata.conf
-[ml]
-    enable statistics charts = yes
-EOT
+#echo 'creating netdata.conf'
+#sudo cat <<EOT > /etc/netdata/netdata.conf
+#[ml]
+#    enable statistics charts = yes
+#EOT
 
-sudo systemctl restart netdata
+#sudo systemctl restart netdata
 
 echo "######################################"
-echo "# FINISHED!!"
+echo "# FINISHED!"
 echo "######################################"
